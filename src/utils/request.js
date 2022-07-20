9279be9dafd05f0405e4864535f2ee84fe82f2d8
Ayoo <Hazell55@163.com>
2022-07-20 17:34:06 +0800

    处理axios的响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,29 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 // create an axios instance
-const service = axios.create()
+const service = axios.create({
+  // 在开发环境中，获取到的是 /api ，会开一个反向代理
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
 service.interceptors.request.use()
-service.interceptors.response.use()
+// 相应拦截器
+service.interceptors.response.use(
+  // 相应成功
+  response => {
+    // 结构三个数据，因为 axios 默认在数据外层加了 data
+    const { message, data, success } = response.data
+    // 如何 success 为true，说明响应成功，则返回响应的 data 数据
+    if (success) {
+      return data
+    } else { // success 为 fales，相应失败，则提示一个错误消息，并且promise.reject进入错误阶段
+      Message.error(message)
+      return Promise.reject(new Error(message))
+    }
+  },
+  // 响应失败
+  error => {
+    Message.error(error.message)
+    return Promise.reject(error)
+  })
 export default service
