45670387874cd57a39f503edf5a3e4b1e23a8dda
Ayoo <Hazell55@163.com>
2022-07-23 16:00:35 +0800

    删除角色功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -32,9 +32,12 @@
               align="center"
               label="操作"
             >
-              <el-button size="small" type="success">分配权限</el-button>
-              <el-button size="small" type="primary">编辑</el-button>
-              <el-button size="small" type="danger">删除</el-button>
+              <template slot-scope="{row}">
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger" @click.native="deleteRole(row)">删除</el-button>
+              </template>
+
             </el-table-column>
           </el-table>
           <!-- 分页 -->
@@ -81,7 +84,7 @@
 </template>
 
 <script>
-import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { getRoleList, getCompanyInfo, deleteRole } from '@/api/setting'
 import { mapGetters } from 'vuex'
 export default {
   data() {
@@ -118,6 +121,17 @@ export default {
     // 获取公司信息
     async getCompanyInfo() {
       this.formDate = await getCompanyInfo(this.companyId)
+    },
+    // 删除角色
+    async deleteRole(value) {
+      try {
+        await this.$confirm('确认删除该角色吗？')
+        await deleteRole(value.id)
+        this.getRoleList()
+        this.$message.success('删除角色成功')
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 
