8c559e53dc025365f2ab8c22b192aaa50dd78d10
Ayoo <Hazell55@163.com>
2022-07-23 19:44:17 +0800

    组件统一注册

--- src/main.js
+++ src/main.js
@@ -18,6 +18,8 @@ import '@/permission' // permission control
 // import *  as  变量  得到的是一个对象{ 变量1：对象1，变量2： 对象2 ...   }, 所以可以采用对象遍历的方法进行处理
 import * as directives from '@/directives'
 
+import Component from '@/components'
+
 Object.keys(directives).forEach(key => {
   Vue.directive(key, directives[key])
 })
@@ -26,7 +28,7 @@ Object.keys(directives).forEach(key => {
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
-
+Vue.use(Component)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      1
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  components: {
+  }
 }
 </script>
 
