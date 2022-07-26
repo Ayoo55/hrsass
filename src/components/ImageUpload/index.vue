58710bf2c945adfd7227c43f199294ed1653f45f
Ayoo <Hazell55@163.com>
2022-07-26 20:31:46 +0800

    上传的进度条显示

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -14,6 +14,7 @@
     >
       <i class="el-icon-plus" />
     </el-upload>
+    <el-progress v-if="showPercent" :percentage="percent" style="width:180px" />
     <el-dialog :visible.sync="showDialog">
       <img :src="imgUrl" alt="" width="100%">
     </el-dialog>
@@ -40,7 +41,9 @@ export default {
       fileList: [{ url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp02%2F1Z9191923035R0-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1661414627&t=3a717bff33a368b195f1871735aca44b' }],
       showDialog: false,
       imgUrl: '',
-      currentFileUid: null
+      currentFileUid: null,
+      percent: 0,
+      showPercent: false
     }
   },
 
@@ -77,6 +80,7 @@ export default {
         return false // 上传终止
       }
       this.currentFileUid = file.uid
+      this.showPercent = true
       return true
     },
     // 添加文件,on-change,文件状态改变时的钩子，添加文件、上传成功和上传失败时都会被调用
@@ -92,7 +96,10 @@ export default {
           Region: 'ap-nanjing',
           Key: params.file.name,
           Body: params.file,
-          StorageClass: 'STANDARD'
+          StorageClass: 'STANDARD',
+          onProgress: (params) => {
+            this.percent = params.percent * 100
+          }
         }, (err, data) => {
           console.log(err || data)
           //   说明上传成功
@@ -104,6 +111,10 @@ export default {
               return item
             })
           }
+          setTimeout(() => {
+            this.showPercent = false
+            this.percent = 0
+          }, 1000)
         })
       }
     }
