3b7efcf7348a061c97799e6a166e26cf5e76894e
Ayoo <Hazell55@163.com>
2022-07-27 16:23:56 +0800

    新建权限点管理页面

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <PageTools>
+        <el-button slot="after" type="primary" size="small">添加权限</el-button>
+      </PageTools>
+      <el-table border>
+        <el-table-column align="center" label="菜单名称" />
+        <el-table-column align="center" label="权限标识" />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作">
+          <el-button type="text">添加</el-button>
+          <el-button type="text">编辑</el-button>
+          <el-button type="text">删除</el-button>
+        </el-table-column>
+
+      </el-table>
     </div>
   </div>
 </template>
