ad3a4e0773a67ebc61d69a444361fdef8eb2e844
Ayoo <Hazell55@163.com>
2022-07-29 09:28:41 +0800

    实现页面的全屏功能

---
+++ src/components/ScreenFull/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <svg-icon icon-class="fullscreen" style="color:#fff; width: 20px; height: 20px" @click="changeScreen" />
+  </div>
+</template>
+
+<script>
+import ScreenFull from 'screenfull'
+export default {
+
+  name: '',
+
+  components: {},
+
+  props: {},
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {
+    changeScreen() {
+      if (!ScreenFull.isEnabled) {
+        this.$message.warning('此时全屏组件不可用')
+        return
+      }
+      ScreenFull.toggle()
+    }
+  }
+}
+</script>
+<style>
+
+</style>
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,6 +8,7 @@
     <!-- <breadcrumb class="breadcrumb-container" /> -->
 
     <div class="right-menu">
+      <ScreenFull class="right-menu-item" />
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img v-imagerror="defalutImg" :src="staffPhoto" class="user-avatar">
@@ -124,7 +125,7 @@ export default {
       height: 100%;
       font-size: 18px;
       color: #5a5e66;
-      vertical-align: text-bottom;
+      vertical-align: middle;
 
       &.hover-effect {
         cursor: pointer;
